85d4da5076904299303e9cfa5e7343c6dc30dce9
EduardoFernandes1002 <FernandesEduardo1002@gmail.com>
2024-10-24 17:07:12 -0300

    SAve

--- js/login.js
+++ js/login.js
@@ -1,47 +1,38 @@
-const apelidos = ['Eduardo', 'Thiago'];
-const emails = ['duduzebas@gmail.com', 'thiagodossantos461@gmail.com'];
-
-
+const apelidos = ["Eduardo", "Thiago"];
+const emails = ["duduzebas@gmail.com", "thiagodossantos461@gmail.com"];
 
 function processarLogin(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Pega os valores dos INPUTS
-    let emailOrUser = document.getElementById('email_user').value;
-    let password = document.getElementById('password').value;
+  // Pega os valores dos INPUTS
+  let emailOrUser = document.getElementById("email_user").value;
+  let password = document.getElementById("password").value;
 
-    // Verifica se o nome ou email predefinidos foram digitados após o botão de submit
-    if (emailOrUser === apelidos[0] || emailOrUser === emails[0]) {
-        
-        // Define uma senha fixa caso o nome ou email predefinido
-        let correctPassword = "12345678";
+  // Verifica se o nome ou email predefinidos foram digitados após o botão de submit
+  if (emailOrUser === apelidos[0] || emailOrUser === emails[0]) {
+    // Define uma senha fixa caso o nome ou email predefinido
+    let correctPassword = "12345678";
 
-        // Valida se a senha digitada é igual a senha pré-definida
-        if (password != correctPassword) {
-            document.getElementById('password').value = "";
-            alert("Erro: Senha Incorreta")
-            return false;
-        };
-
-    } else if (emailOrUser == apelidos[1] || emailOrUser == emails[1]) {
-    
-        // Define uma senha fixa caso o nome ou email pré-definido
-        let correctPassword = "87654321";
-
-        // Valida se a senha digitada é igual a senha pré-definida
-        if (password != correctPassword) {
-            document.getElementById('password').value = "";
-            alert("Erro: Senha Incorreta")
-            return false
-        };
-
-    } else {
-
-        alert("Erro: Email ou Apelido não encontrado")
-        return false
-
+    // Valida se a senha digitada é igual a senha pré-definida
+    if (password != correctPassword) {
+      document.getElementById("password").value = "";
+      alert("Erro: Senha Incorreta");
+      return false;
     }
+  } else if (emailOrUser == apelidos[1] || emailOrUser == emails[1]) {
+    // Define uma senha fixa caso o nome ou email pré-definido
+    let correctPassword = "87654321";
 
-    return window.location.href = 'index.html'
+    // Valida se a senha digitada é igual a senha pré-definida
+    if (password != correctPassword) {
+      document.getElementById("password").value = "";
+      alert("Erro: Senha Incorreta");
+      return false;
+    }
+  } else {
+    alert("Erro: Email ou Apelido não encontrado");
+    return false;
+  }
 
+  return (window.location.href = "index.html");
 }
--- js/registro.js
+++ js/registro.js
@@ -1,33 +1,35 @@
-const apelidosExistentes = ['Thiago', 'Eduardo'];
-const emailsExistentes = ['duduzebas@gmail.com', 'thiagodossantos461@gmail.com'];
+const apelidosExistentes = ["Thiago", "Eduardo"];
+const emailsExistentes = [
+  "duduzebas@gmail.com",
+  "thiagodossantos461@gmail.com",
+];
 
 function processarRegistro(event) {
-    event.preventDefault();
-    let nickname = document.getElementById("nickname").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById('password').value;
-    let confirmarSenha = document.getElementById('passwordConfirm').value;
+  event.preventDefault();
+  let nickname = document.getElementById("nickname").value;
+  let email = document.getElementById("email").value;
+  let senha = document.getElementById("password").value;
+  let confirmarSenha = document.getElementById("passwordConfirm").value;
 
-    if (emailsExistentes.includes(email)) {
-        alert("Erro: Esse email já está em uso!");
-        return false;
-    }
+  if (emailsExistentes.includes(email)) {
+    alert("Erro: Esse email já está em uso!");
+    return false;
+  }
 
-    if (apelidosExistentes.includes(nickname)) {
+  if (apelidosExistentes.includes(nickname)) {
+    let nickname = (document.getElementById("nickname").value = "");
 
-        let nickname = document.getElementById("nickname").value = '';
+    alert("Erro: Esse nickname já está em uso!");
+    return false;
+  }
 
-        alert("Erro: Esse nickname já está em uso!");
-        return false;
-    }
+  if (senha !== confirmarSenha) {
+    alert("Erro: As senhas não correspondem.");
 
-    if (senha !== confirmarSenha) {
-        alert("Erro: As senhas não correspondem.");
+    document.getElementById("password").value = "";
+    document.getElementById("passwordConfirm").value = "";
 
-        document.getElementById('password').value = '';
-        document.getElementById('passwordConfirm').value = '';
-
-        return false;
-    }
-    return window.location.href = 'index.html'
+    return false;
+  }
+  return (window.location.href = "index.html");
 }
